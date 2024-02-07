cc38e39d42bebba3384503c4f8cd0dfe6590ce3d
devsolary <mathewsolomon91@gmail.com>
2024-02-07 16:03:05 +0100

    making about page responsive for desktop view

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -6,8 +6,8 @@ const  Feedback= () => {
 
 
     const divs = [<div key={1} ><div  className="flex flex-col mx-5 pr-20 bg-[#334e6b] pt-10 pb-10 pl-6 w-80"> 
-        <h1>Fantastic! Great Instructor!</h1>
-        <p className="whitespace-normal text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem cumque quod fugit magni esse, aperiam commodi </p>
+        <h1 className="font-extrabold text-lg">Fantastic! Great Instructor!</h1>
+        <p className="whitespace-normal text-lg">It a great life time experience having to take your lecture! it have really been life helping experience. Thanks Alot for your lectures. </p>
     </div>
     <div className="flex flex-row mt-10">
         <img src={jamesImg} alt="" className="h-20 rounded-full ml-10" />
@@ -22,8 +22,8 @@ const  Feedback= () => {
         
     </div>
     </div>, <div key={2}> <div  className="flex flex-col mx-5 pr-20 bg-[#334e6b] pt-10 pb-10 pl-6 w-80">
-                <h1>Fantastic! Great Instructor!</h1>
-                <p className="flex whitespace-normal  text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem cumque quod fugit magni esse, a</p>
+                <h1 className="font-extrabold text-lg">Such a great and incredible Lesson</h1>
+                <p className="flex whitespace-normal  text-xl">I recomend this courses to everyone , and wish you guys luck with the New Studies. i highly recommended this to everyone.</p>
             </div>
             <div  className="flex flex-row mt-10">
             <img src={monicaImg} alt="" className="h-20 rounded-full ml-10" />
@@ -37,8 +37,8 @@ const  Feedback= () => {
         </span>
             </div>
             </div>, <div key={3} ><div  className="flex flex-col mx-5 pr-20 bg-[#334e6b] pt-10 pb-10 pl-6 w-80">
-                <h1>Fantastic! Great Instructor!</h1>
-                <p className="whitespace-normal  text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem cumque quod fugit magni esse, </p>
+                <h1 className="font-extrabold text-lg">I Enjoyed Every Lesson!</h1>
+                <p className="whitespace-normal  text-xl">I am grateful for your wonderful course! Your tutors are the best and i am completely satisfied with the level of professional teaching </p>
             </div>
             <div  className="flex flex-row mt-10">
             <img src={eleanorImg} alt="" className="h-20 rounded-full ml-10" />
@@ -56,7 +56,8 @@ const  Feedback= () => {
         <>
         <div className="bg-[#1B3A5B] pl-5 pr-5 text-white ">
         <div className="pt-10 ">
-            <h3 className="text-xl font-bold">Feedback</h3>
+            <h3 className="text-xl font-bold pb-4">Feedback</h3>
+            <p className="bg-white ml-0 h-1 w-10 pt-0"></p>
             <h1 className="text-4xl font-bold pt-5 pb-10">What Student Say</h1>
         </div>
         <div className="flex flex-row overflow-x-auto no-scrollbar">
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -6,15 +6,16 @@ const AboutUs = () => {
 
     return ( <>
     <div className="bg-white">
-    <div className="pt-10 pb-10 pl-5 bg-blue-600">
-        <h1 className="text-4xl font-extrabold text-white">About Us</h1>
+    <div className="pt-10 pb-10 pl-5 bg-[#153456] lg:pt-28 lg:pb-28 lg:pl-20">
+        <h1 className="text-4xl font-extrabold text-white pb-5">About Us</h1>
         <p className="text-white"><span>Home</span> / <span>About Us</span></p>
     </div>
-    <div className=" pl-5 pr-5">
+    <div className=" pl-5 pr-5 lg:flex lg:flex-row lg:px-20">
         <div className="pt-40">
-            <h3 className="text-l font-bold pb-5 border-b border-yellow-600 w-20">About Us</h3>
-            <h1 className="text-4xl font-extrabold pt-10">Welcome to Doyen Emerging Technology (DoyeneTech)!</h1>
-            <p className="pt-5">DoyeneTech is here to give you knowledge that you need in this competing global economy at the convenience of your chosen place and pace at your level while still maintaining the trend.
+            <h3 className="text-l font-bold pb-4 w-20 lg:text-xl lg:w-40 text-[#07294d]">About Us</h3>
+            <p className="bg-yellow-500 ml-1 h-1 w-10 pt-0 lg:ml-0"></p>
+            <h1 className="text-4xl font-extrabold pt-10 text-[#23252a] lg:text-5xl  lg:w-3/5">Welcome to Doyen Emerging Technology (DoyeneTech)!</h1>
+            <p className="pt-5  lg:w-9/12 text-[#505050]">DoyeneTech is here to give you knowledge that you need in this competing global economy at the convenience of your chosen place and pace at your level while still maintaining the trend.
                 <br />
                 <br />
             DoyeneTech is designed to build for yourself, a profitable and enduring future through informed and seamless means and technologies with assistance of the emerging technology tutors.
@@ -25,50 +26,68 @@ const AboutUs = () => {
                 <br />
             Distance is no more a barrier to a quality learning.</p>
             <br />
-            <button className="p-5 bg-yellow-500">Explore More</button>
+            <button className="p-5 bg-yellow-500 rounded lg:py-3 text-[#07294d] font-bold lg:mt-10">Explore More</button>
         </div>
-        <img src={aboutImg1} alt="" className="w-70 h-40 pl-40"/>
+        <img src={aboutImg1} alt="" className="w-70 h-40 pl-40 lg:mt-80 lg:h-80 lg:pl-0"/>
     </div>
-    <div className="pl-5 pr-5 pt-20 pb-20">
-        <div>
+    <div className="pl-5 pr-5 pt-20 pb-20 lg:flex lg:flex-row lg:mx-20">
+        <div className="lg:w-3/5 lg:mr-10">
             <h1 className="text-5xl text-gray-400">01</h1>
             <h2 className="text-2xl font-extrabold">Why Choose Us</h2>
             <br />
-            <p className="text-gray-600">Your certificate will be globally recognised and the skills will be applicable anywhere in the world.
-            There are qualified and adept professionals selected from all over the globe. Also, they are passionate about teaching.
-            There are wide array of academic and skill development programmes with alumni support.</p>
+            <ul className="list-disc">
+                <li>
+                    <p className="text-gray-600">Your certificate will be globally recognised and the skills will be applicable anywhere in the world.</p>
+                </li>
+                <li>
+                    <p className="text-gray-600">
+                    There are qualified and adept professionals selected from all over the globe. Also, they are passionate about teaching.
+                    </p>
+                </li>
+                <li>
+                    <p className="text-gray-600">
+                    There are wide array of academic and skill development programmes with alumni support.
+                    </p>
+                </li>
+            </ul>
         </div>
         <br />
-        <div>
+        <div className="lg:w-3/5 lg:mr-10">
             <h1 className="text-5xl text-gray-400">02</h1>
             <h2 className="text-2xl font-extrabold">Our Mission</h2>
-            <ul>
+            <br />
+            <ul className="list-disc">
                 <li className="text-gray-600">Our mission is to teach learners at various levels, prepare students for various tests, and offer extensive training on various skills.</li>
                 <li className="text-gray-600">Our mission also includes provision of accessibility to quality and learning opportunities as well as scholarships and Job opportunities.</li>
             </ul>
         </div>
         <br />
-        <div>
+        <div className="lg:w-3/5">
             <h1 className="text-5xl text-gray-400">03</h1>
             <h2 className="text-2xl font-extrabold">Our Vision</h2>
+            <br />
             <p className="text-gray-600">Our vision is to become a leading online school globally.</p>
         </div>
     </div>
     <div className="bg-[#36516E] text-center items-center">
         <div className="pt-10 pb-10">
-            <h1 className="text-3xl text-white font-bold pb-4">1250+</h1>
+            <h1 className="text-3xl text-white font-bold pb-2">1250+</h1>
+            <p className="bg-yellow-500 ml-44 h-1 w-14 pt-0"></p>
             <p className="text-white pt-2">Student Enrolled</p>
         </div>
         <div className="pt-10 pb-10">
             <h1  className="text-3xl text-white font-bold pb-4">512+</h1>
+            <p className="bg-yellow-500 ml-44 h-1 w-14 pt-0"></p>
             <p className="text-white pt-2">People Certified</p>
         </div>
         <div className="pt-10 pb-10">
             <h1  className="text-3xl text-white font-bold pb-4">890+</h1>
+            <p className="bg-yellow-500 ml-44 h-1 w-14 pt-0"></p>
             <p className="text-white pt-2">Course Uploaded</p>
         </div>
         <div className="pt-10 pb-10">
             <h1   className="text-3xl text-white font-bold pb-4">170+</h1>
+            <p className="bg-yellow-500 ml-44 h-1 w-14 pt-0"></p>
             <p className="text-white pt-2">Global Teachers</p>
         </div>
     </div>
